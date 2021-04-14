d12a7792f6ed41d8e253a70881c20934de96a0bf
rajul07 <78417437+rajul07@users.noreply.github.com>
2021-04-14 23:52:24 +0800

    ExpressionEvaluator Initial Commit
    Expression Evaluator to calculate value of instruction from input file

---
+++ ExpressionEvaluator/Calculator.cs
@@ -0,0 +1,78 @@
+﻿
+
+#region "Imports"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+#endregion
+
+#region "namespace"
+namespace ExpressionEvaluator
+{
+    /// <summary>
+    /// Calculation Class
+    /// </summary>
+    public class Calculator
+    {
+        public static Dictionary<long, string> input;
+        public static Dictionary<long, long> Result;
+
+        /// <summary>
+        /// Main Method
+        /// </summary> 
+        public static void Main(string[] args)
+        {
+            input = new Dictionary<long, string>();
+            Result = new Dictionary<long, long>();
+            string[] lines = File.ReadAllLines("input.txt");
+            input = lines.Select(line => line.Split(':')).ToDictionary(keyValue => Convert.ToInt64(keyValue[0]), bits => bits[1]);
+            var key = input.Take(1).Select(d => d.Key).First();
+            var result = CalculateFinalResult(key);
+            Console.WriteLine("The Overall Result is: " + result);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Method to CalculateFinalResult from initial key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static long CalculateFinalResult(long key)
+        {
+            long result = 0;
+            //Looking into Pre Calculated Value
+            if (Result.ContainsKey(key))
+            {
+                return Result[key];
+            }
+            if (input.ContainsKey(key))
+            {
+                string s = input[key];
+                string[] sArr = s.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                if (sArr[0].Equals("Add"))// if the operation is Add
+                {
+                    for (int i = 1; i < sArr.Length; i++)
+                    {
+                        result += CalculateFinalResult(Int64.Parse(sArr[i]));
+                    }
+                }
+                else if (sArr[0].Equals("Mult"))// if the operation is Mult
+                {
+                    result = 1;
+                    for (int i = 1; i < sArr.Length; i++)
+                    {
+                        result *= CalculateFinalResult(Int64.Parse(sArr[i]));
+                    }
+                }
+                else
+                {
+                    result = Int64.Parse(sArr[1]);
+                }
+                Result[key] = result;  // saving the Value
+            }
+            return result;
+        }
+    }
+}
+#endregion
---
+++ ExpressionEvaluatorTest/CalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExpressionEvaluator;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionEvaluatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+      
+        #region TestMethod
+        [TestMethod]
+            public void TestCalculateFinalResult()
+            {
+            long ExpectedResult = 275225993853;
+            Calculator.input = new Dictionary<long, string>();
+            Calculator.Result = new Dictionary<long, long>();
+            string[] lines = File.ReadAllLines("input.txt");
+            Calculator.input = lines.Select(line => line.Split(':')).ToDictionary(keyValue => Convert.ToInt64(keyValue[0]), bits => bits[1]);
+            long key = Calculator.input.Take(1).Select(d => d.Key).First();
+            long ActualResult =  Calculator.CalculateFinalResult(key);
+
+            Assert.AreEqual(ExpectedResult, ActualResult);
+            }
+
+        #endregion
+
+    }
+
+}
